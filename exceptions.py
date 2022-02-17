a810f43bc20496cf13717e1047c52c25784c3f8b
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-02-17 22:19:09 +0100

    Create exceptions.py

---
+++ exceptions.py
@@ -0,0 +1,8 @@
+class PasswordException(BaseException):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+class AccountException(BaseException):
+    def __init__(self) -> None:
+        super().__init__()
